fa307daea7a8e079e433839129d0830d4366abd9
indy <dork@indypaige.dev>
2026-06-04 13:37:31 -0500

    fmt

--- lib/haskell.nix
+++ lib/haskell.nix
@@ -3,19 +3,19 @@
 , ...
 }:
 
-{ ghcv ? "9124"
-, name
-, root
-, ...
-}:
-
-{
-  mk = let ghcr = "ghc${ghcv}";
-           ghcp = pkgs.haskell.packages.${ghcr};
-           ghcc = pkgs.haskell.compiler.${ghcr};
-           tool = [ ghcc ghcp.cabal-install pkgs.hpack ];
-       in ghcp.developPackage
-         { modifier = drv: pkgs.haskell.lib.addBuildTools drv tool;
-           inherit name root;
-         };
+rec {
+  defaultGhcv = "9124";
+  mk          = { ghcv ? defaultGhcv
+                , name
+                , root
+                , ...
+                }:
+                  let ghcr = "ghc${ghcv}";
+                      ghcp = pkgs.haskell.packages.${ghcr};
+                      ghcc = pkgs.haskell.compiler.${ghcr};
+                      tool = [ ghcc ghcp.cabal-install pkgs.hpack ];
+                  in ghcp.developPackage
+                    { modifier = drv: pkgs.haskell.lib.addBuildTools drv tool;
+                      inherit name root;
+                    };
 }
