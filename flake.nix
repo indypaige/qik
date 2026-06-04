1b4d1059a737718a2984a122839c0e98c34e8cb0
indy <dork@indypaige.dev>
2026-06-04 13:34:35 -0500

    lib haskell

--- flake.nix
+++ flake.nix
@@ -14,6 +14,6 @@
         let pkgs =    import nixpkgs { inherit system;              };
             load = l: import l       { inherit system pkgs nixpkgs; };
         in
-          { lib.haskell = load ./mk/haskell.nix;
+          { lib.haskell = load ./lib/haskell.nix;
           });
 }
